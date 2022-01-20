8e7a84375fcb703de52597712ad79130009aacd6
sanjai kumar n <nsksanjai@gmail.com>
2022-01-20 15:20:18 +0530

    finel change

--- assets/js/simpleparallax.js
+++ assets/js/simpleparallax.js
@@ -1,7 +1,7 @@
 
 const simpleParallax = require('simple-parallax-js');
 
-const image = document.getElementsByClassName("whyus");
+const image = document.getElementsByClassName("hero-h1");
 new simpleParallax(image, {
 	delay: .6,
 	transition: 'cubic-bezier(0,0,0,1)',
