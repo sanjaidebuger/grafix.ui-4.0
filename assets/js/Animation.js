b794317a56b5a3a327d225a3491fa9ccd479aa83
sanjai kumar n <nsksanjai@gmail.com>
2022-01-24 08:04:12 +0530

    animation update

--- assets/js/Animation.js
+++ assets/js/Animation.js
@@ -4,7 +4,6 @@ let service = document.getElementById("service");
 window.addEventListener('scroll',function() {
 var value = window.scrollY;
 
-home.style.top = value * 0.1 + 'px';
-service.style.top = value * 0.1 + 'px';
+
 
 })
